b8110c82a9b31affd3177c60f7a3fb4e7bd156d7
Cullain <lukas.gedrat@gmx.net>
2023-05-22 08:55:43 +0200

    Eigener Startcode

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,9 +1,10 @@
 package com.cc.java;
 
-public class App {
+public class App 
+{
     
     public static void main(String[] args) {
-       System.out.println("Hello, World!");
+        System.out.println("Good Morning!");
     }
-    
+     
 }
